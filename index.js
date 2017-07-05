a1af30693755b7d6ada78ed0d43506800b04818c
Maike Mota <maike_henrique@hotmail.com>
2017-07-05 19:50:25 -0300

    created processBody method to handle the body of the request and extract the ebook data

--- index.js
+++ index.js
@@ -10,23 +10,19 @@ const packtpubFreeLearningUrl = 'https://www.packtpub.com/packt/offers/free-lear
 console.log(`----- Start requesting book name from packtpub (${new Date().toLocaleString()}) -----`);
 
 get(packtpubFreeLearningUrl).then((body) => {
-    var $ = cheerio.load(body);
-    let bookName = $('.dotd-title').find('h2').text().trim();
-    let bookCover = $('.bookimage')[0].attribs.src;
-    console.log("Book of the Day: " + bookName);
-    inform(config.receiverId, bookName, bookCover);
-    console.log(`----- End requesting book name from packtpub (${new Date().toLocaleString()}) -----`);
-}).catch(error => {
-    console.error('Error while requesting book name.');
-    console.error(error);
+    let ebookData = processBody(body);
+    console.log("Book of the Day: " + ebookData.name);
+    inform(config.receiverId, ebookData.name, ebookData.cover).then(() => {
+        console.log(`----- End requesting book name from packtpub (${new Date().toLocaleString()}) -----`);
+        process.exit(0);
+    });
 });
-
 
 /**
  * Wraps the request from node in a promise
  * @param {String} targetUrl the target Url
  */
-function get(targetUrl) {
+async function get(targetUrl) {
     return new Promise((resolve, reject) => {
         request(targetUrl, function (error, response, body) {
             if (!error && response.statusCode == 200) {
@@ -39,8 +35,25 @@ function get(targetUrl) {
 }
 
 /**
+ * Process the body of the page
+ * @param {String} body the body to be processed
+ * @returns {BookData} An object containing the ebook data.
+ */
+function processBody(body) {
+    let $ = cheerio.load(body);
+    let bookName = $('.dotd-title').find('h2').text().trim();
+    let bookCover = $('.bookimage')[0].attribs.src;
+    if (bookCover.startsWith('//')) {
+        bookCover = `https:${bookCover}`;
+    }
+    return {
+        'name': bookName,
+        'cover': bookCover
+    }
+}
+
+/**
  * Notify the receiver about the book of the day
- * 
  * @param {String} chatId The id of the chat with the user/group.
  * @param {String} bookName The name of the book.
  * @param {String} bookCover The link to the book cover.
@@ -49,7 +62,8 @@ async function inform(chatId, bookName, bookCover) {
     await bot.sendChatAction(chatId, 'typing');
     await bot.sendMessage(chatId, `Sr, the book of the day: <strong>${bookName}</strong>`, { "parse_mode": "html" });
     await bot.sendChatAction(chatId, 'upload_photo');
-    await bot.sendPhoto(chatId, `${bookCover}`);
+    let encodedUrl = encodeURI(bookCover);
+    await bot.sendPhoto(chatId, `${encodedUrl}`, { "caption": bookName });
     await bot.sendChatAction(chatId, 'typing');
-    await bot.sendPhoto(chatId, `<a href='${packtpubFreeLearningUrl}'></a>`, { "parse_mode": "html" });
+    await bot.sendMessage(chatId, `<a href='${packtpubFreeLearningUrl}'>Claim it at packetpub!</a>`, { "parse_mode": "html" });
 }
